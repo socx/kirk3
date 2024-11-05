6070ebded705d668287bf8f9371e05a195d16df1
Socx Musterion <socx9@yahoo.com>
2024-11-05 18:11:12 +0100

    [WEB] clean up codebase

--- estate-portfolio-web/src/pages/PageNotFound.jsx
+++ estate-portfolio-web/src/pages/PageNotFound.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import Sidebar from '../../partials/Sidebar';
-import Header from '../../partials/Header';
+import Sidebar from '../partials/Sidebar';
 
-import NotFoundImage from '../../images/404-illustration.svg';
-import NotFoundImageDark from '../../images/404-illustration-dark.svg';
+import PageHeader from '../components/PageHeader'
+
+import NotFoundImage from '../images/404-illustration.svg';
+import NotFoundImageDark from '../images/404-illustration-dark.svg';
 
 function PageNotFound() {
 
@@ -19,7 +20,7 @@ function PageNotFound() {
       {/* Content area */}
       <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden bg-white dark:bg-gray-900">
         {/*  Site header */}
-        <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} variant="v3" />
+        <PageHeader sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} variant="v3" />
 
         <main className="grow">
           <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
@@ -30,7 +31,7 @@ function PageNotFound() {
                   <img className="hidden dark:block" src={NotFoundImageDark} width="176" height="176" alt="404 illustration dark" />
                 </div>
                 <div className="mb-6">Hmm...this page doesn’t exist. Try searching for something else!</div>
-                <Link to="/" className="btn bg-gray-900 text-gray-100 hover:bg-gray-800 dark:bg-gray-100 dark:text-gray-800 dark:hover:bg-white">
+                <Link to="/dashboard" className="btn bg-gray-900 text-gray-100 hover:bg-gray-800 dark:bg-gray-100 dark:text-gray-800 dark:hover:bg-white">
                   Back To Dashboard
                 </Link>
               </div>
