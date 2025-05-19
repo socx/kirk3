a51777906f8cae7d12e9278f5ec0d6d94852334b
Socx Musterion <socx9@yahoo.com>
2025-05-19 17:59:17 +0100

    [API] possible changes to testing set up

--- kirk-api/jest.config.mjs
+++ kirk-api/jest.config.mjs
@@ -7,4 +7,12 @@ export default {
   },
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$",
   moduleFileExtensions: ["ts", "js", "json", "node"],
+  // projects: [
+  //   {
+  //     testPathIgnorePatterns: ["<rootDir>/node_modules/"],
+  //     preset: "ts-jest",
+  //     displayName: "my-package",
+  //     testMatch: ["<rootDir>/packages/my-package/__tests__/**/*.spec.ts"],
+  //   }
+  // ],
 };
